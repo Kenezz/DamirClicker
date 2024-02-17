02504134d0a576490af94647d11a15f15de32d67
Kenezz <118610779+Kenezz@users.noreply.github.com>
2024-02-17 22:12:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let clickerLevel = document.querySelector('.clicker-level')
 let clickerIncrease = document.querySelector('.clicker-increase')
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
+let markCost = document.querySelector('.mark-cost')
+let parsedMarkCost = parseFloat(markCost.innerHTML)
+let markLevel = document.querySelector('.mark-level')
+let markIncrease = document.querySelector('.mark-increase')
+let parsedMarkIncrease = parseFloat(markIncrease.innerHTML)
+
 let gpc = 1;
 
 function incrementDamir() {
@@ -26,6 +32,24 @@ function buyClicker() {
 
         parsedClickerCost *= 1.18;
         clickerCost.innerHTML = Math.round(parsedClickerCost)
+    }
+
+
+
+}
+
+function buyMark() {
+    if (parsedDamir >= parsedMarkCost) {
+        damir.innerHTML = Math.round(parsedDamir -= parsedMarkCost)
+
+        markLevel.innerHTML ++
+
+        parsedMarkIncrease = parseFloat((parsedMarkIncrease * 1.03).toFixed(2))
+        markIncrease.innerHTML = parsedMarkIncrease
+        gpc += parsedMarkIncrease
+
+        parsedMarkCost *= 1.18;
+        markCost.innerHTML = Math.round(parsedMarkCost)
     }
 
 
